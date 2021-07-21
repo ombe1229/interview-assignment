33f6dd87e550b330ef2bb0333eb308034a690515
ombe <h3236516@gmail.com>
2021-07-22 03:25:58 +0900

    style(bot): apply black style

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import cogs
 class Bot(commands.Bot):
     def __init__(self, **kwargs):
         super().__init__(command_prefix="!", help_command=None, **kwargs)
-        
+
         cogs.load(self)
 
     async def on_ready(self):
@@ -16,9 +16,9 @@ class Bot(commands.Bot):
     async def on_command_error(self, ctx: commands.Context, exception: Exception):
         if isinstance(exception, commands.CommandNotFound):
             return
-        
+
         return await ctx.send(f"Error: {exception}")
 
-    
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     Bot().run(os.environ.get("TOKEN"))
