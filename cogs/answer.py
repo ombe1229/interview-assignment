5d1c26d156d5b052f0fc4171720bf9fb721674e9
Restyled.io <commits@restyled.io>
2021-07-21 17:15:52 +0000

    Restyled by autopep8

--- cogs/answer.py
+++ cogs/answer.py
@@ -6,6 +6,7 @@ import discord
 if TYPE_CHECKING:
     from bot import Bot
 
+
 class Answer(commands.Cog):
 
     def __init__(self, bot: "Bot"):
@@ -15,23 +16,25 @@ class Answer(commands.Cog):
     async def todo(self, ctx, query=0):
 
         def add_submitted(i, submitted):
-            embed.add_field(name=f'**{i+1}. {submitted.id}**', value=f'{datetime.fromtimestamp(submitted.date)}\n[초대 링크](https://discord.com/oauth2/authorize?client_id={submitted.id}&scope=bot&guild_id=653083797763522580)', inline=False)
+            embed.add_field(name=f'**{i+1}. {submitted.id}**',
+                            value=f'{datetime.fromtimestamp(submitted.date)}\n[초대 링크](https://discord.com/oauth2/authorize?client_id={submitted.id}&scope=bot&guild_id=653083797763522580)', inline=False)
 
-        embed = discord.Embed(title=':rocket: ToDo List', color=discord.Color.green(), description='_ _')
-        
+        embed = discord.Embed(title=':rocket: ToDo List',
+                              color=discord.Color.green(), description='_ _')
+
         if not query:
             for i, submitted in enumerate(submits):
                 add_submitted(i, submitted)
-        
+
         else:
             for i, submitted in enumerate(submits):
                 if submitted.id == query:
                     break
-                if i == len(submits): # query가 일치하는 id가 없을 경우 인덱스로 인식
+                if i == len(submits):  # query가 일치하는 id가 없을 경우 인덱스로 인식
                     add_submitted(i, submits[query-1])
 
             add_submitted(i, submitted)
-        
+
         await ctx.send(embed=embed)
 
     @commands.command()
@@ -42,7 +45,7 @@ class Answer(commands.Cog):
                 del submits[i]
                 await ctx.send(f'**{i+1}. {query}**가 정상적으로 승인되었습니다.')
                 break
-            
+
             if i == len(submits):
                 await ctx.send(f'{query}를 찾을 수 없습니다.')
 
@@ -53,10 +56,10 @@ class Answer(commands.Cog):
                 del submits[i]
                 await ctx.send(f'**{i+1}. {query}**가 정상적으로 거부되었습니다.')
                 break
-            
+
             if i == len(submits):
                 await ctx.send(f'{query}를 찾을 수 없습니다.')
-        
+
 
 def setup(bot: "Bot"):
     bot.add_cog(Answer(bot))
