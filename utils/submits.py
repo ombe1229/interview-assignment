82550a677909f4b1c59460539e962aa5c29d8b60
ombe <h3236516@gmail.com>
2021-07-23 14:29:29 +0900

    style(submits): change return type Noreturn to None

--- utils/submits.py
+++ utils/submits.py
@@ -1,4 +1,4 @@
-from typing import Any, NoReturn
+from typing import Any
 from submits import submits, approved_submits
 
 
@@ -14,19 +14,19 @@ class Queue:
     def items(self):
         return sorted(self.__items, key=lambda item: item["timestamp"])
 
-    def init(self) -> NoReturn:
+    def init(self) -> None:
         self.__items = [{"id": i.id, "timestamp": i.date} for i in submits]
 
     def find_by_id(self, id: int) -> dict[str, Any]:
         return list(filter(lambda item: item["id"] == id, self.__items))[0]
 
-    def approve(self, bot: dict[str, Any]) -> NoReturn:
+    def approve(self, bot: dict[str, Any]) -> None:
         self.__items.remove(bot)
         item = list(filter(lambda item: item.id == bot["id"], submits))[0]
         submits.remove(item)
         approved_submits.append(item)
 
-    def deny(self, bot: dict[str, Any]) -> NoReturn:
+    def deny(self, bot: dict[str, Any]) -> None:
         self.__items.remove(bot)
         item = list(filter(lambda item: item.id == bot["id"], submits))[0]
         submits.remove(item)
